2421817beb56b5250653bc4b3a3f532b10070b55
Le Thanh Nam <lethanhnam330@gmail.com>
2025-09-13 20:53:36 +0700

    complete

---
+++ src/app/(user)/layout.tsx
@@ -0,0 +1,30 @@
+import Box from '@mui/material/Box';
+import Header from '@/components/Header/header';
+import Authorization from '@/components/auth/authorization';
+
+export default function UserLayout({
+    children,
+    header,
+}: {
+    children: React.ReactNode;
+    header: React.ReactNode;
+}) {
+    return (
+        <Authorization>
+            <Box>
+                {/* Parallel route có thể bị mất header bên profile page */}
+                {/* {header} */}
+                <Header />
+                <Box
+                    sx={{
+                        padding: 'calc(var(--height-header) + 16px) 8px 0px',
+                        backgroundColor: '#f0f2f5',
+                        // minHeight: '500vh',
+                    }}
+                >
+                    {children}
+                </Box>
+            </Box>
+        </Authorization>
+    );
+}
